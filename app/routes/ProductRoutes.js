94ac4ee98621a70e112cf0ab8c550359d29d4729
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2024-03-29 17:34:02 -0300

    Update ProductRoutes.js

--- app/routes/ProductRoutes.js
+++ app/routes/ProductRoutes.js
@@ -2,9 +2,11 @@ const bodyParser = require('body-parser');
 
 const saveProductController = require('../controllers/saveProduct.js').saveProduct;
 const getProductController = require('../controllers/getProducts.js').getProducts;
+const deleteProductController = require('../controllers/deleteProduct.js').deleteProduct; 
 
 module.exports = (app) => {
     app.post('/saveProduct', bodyParser.json(), saveProductController);
     app.get('/getProducts', bodyParser.json(), getProductController);
     app.get('/getProduct/:productName', bodyParser.json(), getProductController);
+    app.delete('/deleteProduct/:productName', bodyParser.json(), deleteProductController);
 }
